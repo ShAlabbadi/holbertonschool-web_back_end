fd6b4fbb914023fd8ac85be5626358b896070369
ShAlabbadi <10708@holbertonstudents.com>
2025-08-26 10:39:40 +0000

    5. Typed Arrays

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,12 @@
-export default function createInt8TypedArray(length, position, value){
-	if (position < 0 || position >= length) {
-		throw new Error('Position outside range');
-	}
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
 
-	const buffer = new ArrayBuffer (length);
-	const int8Array = new DataView (buffer);
+	const buffer = new ArrayBuffer(length);
+	const int8Array = new Int8Array(buffer);
 
+	int8Array[position] = value;
 
-	dataView.setInt8(position, value);
-
-	return dataView;
+	return new DataView(buffer);
 }
