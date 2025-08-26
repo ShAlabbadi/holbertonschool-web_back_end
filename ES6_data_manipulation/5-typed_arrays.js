f71261fb85a5035d2cec5d53e4e3de30e1552453
ShAlabbadi <10708@holbertonstudents.com>
2025-08-26 10:35:09 +0000

    5. Typed Arrays

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,7 @@ export default function createInt8TypedArray(length, position, value){
 	}
 
 	const buffer = new ArrayBuffer (length);
-	const int8Array = new int8Array (buffer);
+	const int8Array = new DataView (buffer);
 
 	int8Array[position] = value;
 	return new DataView(buffer);
