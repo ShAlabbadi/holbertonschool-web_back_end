2a441155a112d12513cc71f668ea41a8d397bc19
ShAlabbadi <10708@holbertonstudents.com>
2025-09-02 10:23:15 +0000

    2. Reading a file synchronously with Node JS

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,12 +2,17 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
+    // Read the file synchronously
     const data = fs.readFileSync(path, 'utf8');
+
+    // Split the data into lines and filter out empty lines
     const lines = data.trim().split('\n');
     const students = lines.slice(1).filter(line => line.trim() !== '');
 
+    // Log total number of students
     console.log(`Number of students: ${students.length}`);
 
+    // Group students by field
     const fields = {};
     students.forEach(student => {
       const [firstname, lastname, age, field] = student.split(',');
@@ -17,6 +22,7 @@ function countStudents(path) {
       fields[field].push(firstname);
     });
 
+    // Log number of students and list for each field
     for (const field in fields) {
       if (Object.prototype.hasOwnProperty.call(fields, field)) {
         console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
